74e60a4a3577b2c7559a9f5c0801a858874bb26b
freelanceagdevops <freelanceagdevops@gmail.com>
2024-10-16 12:13:09 +0530

    files added

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  #access_key = "<Your Access Key>"
+  #secret_key = "<Your Secret Key>"
+}
